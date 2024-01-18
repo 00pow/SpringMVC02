01660aa7738fb8806248d8081f1ad63341919342
00pow <kimseo131612@gmail.com>
2024-01-18 23:54:03 +0900

    commit!

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -19,45 +19,10 @@ import kr.board.mapper.BoardMapper;
 @Controller
 public class BoardController {
 
-	@Autowired
-	BoardMapper boardMapper;
 	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
 	}
-	// @ResponseBody->jackson-databind(객체를-> JSON 데이터포멧으로 변환)
-	@RequestMapping("/boardList.do")
-	public @ResponseBody List<Board> boardList(){
-		List<Board> list=boardMapper.getLists();
-		return list;	// @ResponseBody :   JSON 데이터 형식으로 변환(API)해서 리턴(응답)하겠다.
-	}
 	
-	@RequestMapping("/boardInsert.do")
-	public @ResponseBody void boardInsert(Board vo) {
-		boardMapper.boardInsert(vo); // 등록성공
-	}
-	
-	@RequestMapping("/boardDelete.do")
-	public @ResponseBody void boardDelete(@RequestParam("idx") int idx) {
-		boardMapper.boardDelete(idx);
-	}
-	
-	@RequestMapping("/boardUpdate.do")
-	public @ResponseBody void boardUpdate(Board vo) {
-		boardMapper.boardUpdate(vo);
-	}
-	
-	@RequestMapping("/boardContent.do")
-	public @ResponseBody Board boardContent(int idx) {
-		Board vo=boardMapper.boardContent(idx);
-		return vo; // vo->JSON 
-	}
-	
-	@RequestMapping("/boardCount.do")
-	public @ResponseBody Board boardCount(int idx) {
-		boardMapper.boardCount(idx);
-		Board vo = boardMapper.boardContent(idx);
-		return vo;
-	}
 }
