32c10a97443139ee94e10dbe2f5c71d65b0f26cb
00pow <kimseo131612@gmail.com>
2024-01-19 00:02:37 +0900

    commit!

--- SpringMVC02/src/main/java/kr/board/controller/BoardRestController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardRestController.java
@@ -3,6 +3,8 @@ package kr.board.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,13 +21,14 @@ public class BoardRestController {
 		BoardMapper boardMapper;
 	
 		// @ResponseBody->jackson-databind(객체를-> JSON 데이터포멧으로 변환)
-		@RequestMapping("/boardList.do")
+		@GetMapping("/all")
 		public List<Board> boardList(){
 			List<Board> list=boardMapper.getLists();
 			return list;	// @ResponseBody :   JSON 데이터 형식으로 변환(API)해서 리턴(응답)하겠다.
 		}
 		
-		@RequestMapping("/boardInsert.do")
+		//@RequestMapping("/boardInsert.do")
+		@PostMapping("/new")
 		public void boardInsert(Board vo) {
 			boardMapper.boardInsert(vo); // 등록성공
 		}
